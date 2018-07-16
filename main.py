280ea2bf504fd1844be8e2dd97341368e15ff156
Artoym Smolyanchenko <ming67rotten@gmail.com>
2018-07-16 23:31:12 +1000

    Upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import asyncio
+from aiovk import API, ImplicitSession
+from time import sleep
+
+async def eraser():
+    print ("Enter login:")
+    login = input()
+    print ("Enter password:")
+    password = input()
+    print ("Enter secret:")
+    secret = input()
+    
+    session = ImplicitSession(login, password, secret, ['photos', 'wall'])
+    await session.authorize()
+
+    api = API(session)
+
+    while True:
+        wall = await api.wall.get(count = 100)
+        if wall['count'] == 0:
+            break
+        for i in wall['items']:
+            await api.wall.delete(post_id=i['id'])
+            sleep(2)
+
+    while True:
+        photos = await api.photos.get(album_id='wall', count=1000)
+        if photos['count'] == 0:
+            break
+        for i in photos['items']:
+            await api.photos.delete(photo_id=i['id'])
+            sleep(2)
+
+    session.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(eraser())
